521a671b7f648892331d1ca8df1a20757f556c04
Paulo Linhares <plinhares9@gmail.com>
2017-10-19 15:42:18 -0300

    new schema of mask for CPF

--- app/src/main/java/br/com/packapps/cpfandbirthdatemask/MainActivity.java
+++ app/src/main/java/br/com/packapps/cpfandbirthdatemask/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.packapps.cpfandbirthdatemask;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -32,84 +33,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "CPF: "+ etCpf.getText().toString(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                //startActivity(new Intent(MainActivity.this, SecondExampleMaskActivity.class));
             }
         });
 
 
         //mask CPF
-        setMaskCpf();
+        setMaskCpf("###.###.###-##");
 
 
     }
 
-    private void setMaskCpf() {
-        etCpf.addTextChangedListener(new TextWatcher() {
-            boolean isUpdating;
-            String old = "";
-
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String withMask = includeMaskCpf(s.toString(), count);
-                if (isUpdating){
-                    old = withMask;
-                    isUpdating = false;
-                }else{
-                    isUpdating = true;
-                    etCpf.setText(withMask);
-                    etCpf.setSelection(withMask.length());
-                }
-
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
+    private void setMaskCpf(String maskCpf) {
+        etCpf.addTextChangedListener(Mask.insert(maskCpf, etCpf));
     }
-
-    private String includeMaskCpf(String s, int count) {
-        switch (s.length()){
-            case 3:
-                if (count == 0)//necessary in case of deleting
-                    return s.substring(0, 3);
-                else
-                    return s + ".";
-            case 7:
-                if (count == 0)//necessary in case of deleting
-                    return s.substring(0, 7);
-                else
-                    return s + ".";
-            case 11:
-                if (count == 0)//necessary in case of deleting
-                    return s.substring(0, 11);
-                else
-                    return s + "-";
-
-            //### For cases DELETING ###
-
-            case 12://necessary for case to be deleted string after of the character "-" and go back continue digit
-                if (count == 0)//necessary in case of deleting
-                    return s.substring(0, 11);
-
-                if (!s.contains("-"))
-                    return s.substring(0,11) + "-"+ s.substring(11,12);//deleting character "-" and conti
-
-            case 8://necessary for case to be deleted string after of the character "-" and go back continue digit
-                if (count == 0)//deleting
-                    return s.substring(0, 7);
-
-                if (!s.substring(5,8).contains("."))
-                    return s.substring(0,7) + "."+ s.substring(7,8);//deleting character "." and conti
-        }
-
-        return s;
-    }
-
 }
